9415a7138d1027e29a22091f169737c8abfdf360
Ricky Huang <huangricky@gmail.com>
2019-03-17 12:42:12 -0400

    ET712 Midterm Project

---
+++ js/state-data1.js
@@ -0,0 +1,156 @@
+var stateData = {
+  usa: {
+    who: "BLUE NATION",
+    what: "2020 U.S. Presidential Election",
+    when: "Tuesday, November 3, 2020",
+    where: "All 50 States, Duh!",
+    why: "Defeat Trump At All Costs!",
+    how: "Select the Best Candidate!",
+    wow: "What a group of candidates!",
+    whoa: "Get Ready for a Fight DJT!",
+    wait: "Calm Down and...Vote!"
+  },
+  bs: {
+    who: 'Bernie Sanders AKA "Bernie"',
+    what: "Definitely Running",
+    when: "Gathering Forces Now",
+    where: "Vermont Senator",
+    why: "Strong Grassroots Following",
+    how: "$15 Federal Minimum Wage",
+    wow: "Universal Healthcare Reform",
+    whoa: "Campaign Finance Reform",
+    wait: "Democrat When Convenient"
+  },
+  kh: {
+    who: "Kamala Harris",
+    what: "Definitely Running",
+    when: "Gathering Campaign Now",
+    where: "Cali Senator",
+    why: "Charismatic Californian",
+    how: "Represents diversity",
+    wow: "Went Hard on Kavanaugh",
+    whoa: "African-American & Indian-American",
+    wait: "Former Prosecutor"
+  },
+  ew: {
+    who: "Elizabeth Warren",
+    what: "Definitely Running",
+    when: "Gathering Forces Now",
+    where: "Massachusetts Senator",
+    why: "Wants to crack down on Corporations",
+    how: "Wealth Tax",
+    wow: "Consumer Federal Protection Bureau",
+    whoa: "Help Rebuild the Middle Class",
+    wait: "Controversy about Cherokee heritage"
+  },
+  jc: {
+    who: "Julian Castro",
+    what: "Definitely Running",
+    when: "Gathering Volunteers Now",
+    where: "San Antonio, Texas - Mayor",
+    why: "Increasingly Blue Southwest",
+    how: "Antitode to Trump",
+    wow: "Worked under Obama Administration",
+    whoa: "Internet access to public housing",
+    wait: "Violated the Hatch Act in 2016"
+  },
+  bor: {
+    who: "Beto O'Rourke",
+    what: "Probably Running",
+    when: "Gathering Teams Now",
+    where: "El Paso, Texas - Representative",
+    why: "The Man, The Myth, The Legend",
+    how: "Young, Hip, and Inspirational",
+    wow: "Immigration Reform",
+    whoa: "Bass guitarist",
+    wait: "More liberal than record shows"
+  },
+  jb: {
+    who: "Joe Biden",
+    what: "May Be Running",
+    when: "Gathering Resources Now",
+    where: "Former Delaware Senator",
+    why: "Former Vice President",
+    how: "Win back blue-colar voters",
+    wow: "Top Foreign Policy Credentials",
+    whoa: "Congressional Patriot Award",
+    wait: 'Called Mike Pence "a decent guy"'
+  },
+  tg: {
+    who: "Tulsi Gabbard",
+    what: "Definitely Running",
+    when: "Gathering Troopers Now",
+    where: "Honolulu, Hawaii - City Council",
+    why: "Samoan-American & Hindu",
+    how: "Noninterventionist Foreign Policy",
+    wow: "Army National Guard",
+    whoa: "Served in Iraq",
+    wait: "Helped father's anti-gay org."
+  },
+  cb: {
+    who: "Cory Booker",
+    what: "Definitely Running",
+    when: "Gathering Forces Now",
+    where: "New Jersey Senator",
+    why: "Social Media Darling",
+    how: "Rhodes Scholar",
+    wow: "Aims to alleviate poverty",
+    whoa: "Expand afforadable healthcare",
+    wait: "Mixed record N.J. mayor"
+  },
+  ay: {
+    who: "Andrew Yang",
+    what: "Definitely Running",
+    when: "Gathering Support Now",
+    where: "Cali/NY Tech Entrepreneur",
+    why: "Revitalize struggling urban centers",
+    how: "Robust Grassroots Campaign",
+    wow: "Universal Basic Income",
+    whoa: "Freedom Dividend",
+    wait: "At least he ran..."
+  },
+  ji: {
+    who: "Jay Inslee",
+    what: "Definitely Running",
+    when: "Gathering Forces Now",
+    where: "Washington State Governor",
+    why: "Defeat Climate Change",
+    how: "Environmentalist Support",
+    wow: "Washington is EV-friendly",
+    whoa: "Reduced state's use of fossil fuels",
+    wait: "Climate change deniers will oppose"
+  },
+  kg: {
+    who: "Kirsten Gillibrand",
+    what: "Probably Running",
+    when: "Gathering Supporters Now",
+    where: "New York Senator",
+    why: "Feminist Champion",
+    how: "Can relate to red-state Americans",
+    wow: "Fight racism, greed, corruption in Wash.",
+    whoa: "Postal Banking Services",
+    wait: "Went from moderate to liberal"
+  },
+  pb: {
+    who: "Pete Buttigieg",
+    what: "Probably Running",
+    when: "Gathering Resources Now",
+    where: "South Bend, Indiana - Mayor",
+    why: "Could become 1st LGBT President",
+    how: "Fight Corruption in Washington",
+    wow: "National Reserve Veteran",
+    whoa: "Won 3/4 of votes in mayorial election",
+    wait: "Lost in 2010 for Indiana treasurer"
+  },
+  ak: {
+    who: "Amy Klobuchar",
+    what: "Definitely Running",
+    when: "Gathering Support Now",
+    where: "Minnesota Senator",
+    why: 'Practical laws like "ban lead in toys"',
+    how: "Tempered expectations on Green New Deal",
+    wow: "Reduced backlog of rape kits",
+    whoa: "Stern, cool questioning of Brett Kavanaugh",
+    wait: "Accused of being abusive to staff"
+  }
+};
